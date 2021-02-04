a2f4a7f3751c50d716b4d5fdf97be829d5c1b050
Sergio Guillen <sagssky@gmail.com>
2021-02-04 12:22:10 -0600

    Creating project

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace Exercise2_AutomationCSharp
 {
     class Program
     {
+        //Exercise 2
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
         }
